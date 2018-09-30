63fb5f43bed03aca147c831f3a82f5e84009965b
Vladimir <mail@vldmrnine.com>
2018-09-30 22:53:24 +0300

    First challenge

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,1 +1,22 @@
-console.log("Hello world");
+
+var markMass, markHeight, johnMass, johnHeight;
+
+markMass = prompt("Please enter Mark Mass in klgms");
+console.log("Mark mass " + markMass);
+
+markHeight = prompt("Please enter Mark Height in meters");
+console.log("Mark heigth " + markHeight);
+
+johnMass = prompt("Please enter John Mass in klgms");
+console.log("John mass " + johnMass);
+
+johnHeight = prompt("Please enter John Height in meters");
+console.log("John height " + johnHeight);
+
+var markBMI = markMass/(markHeight^2);
+var johnBMI = johnMass/(johnHeight^2);
+console.log(markBMI, johnBMI);
+
+var compareBMI = markBMI > johnBMI;
+
+console.log("Is Mark's BMI higher than John's? " + compareBMI);
